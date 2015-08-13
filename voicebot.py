6ed45e23ab7727380d5e48288b60a1fa4844e712
Steven Van Acker <steven@singularity.be>
2015-08-13 13:09:43 +0200

    list voices

--- voicebot.py
+++ voicebot.py
@@ -17,6 +17,8 @@ FULLNAME = "VoiceBot v0.1"
 BOTURL = "https://github.com/StevenVanAcker/VoiceBot"
 DATAFILE = "/tmp/datafile"
 
+allvoices = "mike crystal claire julia lauren mel ray rich rosa alberto".split(" ")
+
 class VoiceBot(GenericIRCBot):
     def __init__(self): #{{{
         self.catchall = self.handle_catchall
@@ -35,16 +37,32 @@ class VoiceBot(GenericIRCBot):
 		"help": "say something",
 		"msgtypes": ["private", "public", "directed"],
 	    },
+	    "!voices": { 
+	    	"fn": self.handle_VOICES, 
+		"argc": self.DontCheckARGC, 
+		"tillEnd": True,
+		"help": "list voices",
+		"msgtypes": ["private", "public", "directed"],
+	    },
 	}
+    #}}}
+    def handle_VOICES(self, req): #{{{
+	self.sendReply(req, "Voices: %s" % " ".join(allvoices))
+	return
     #}}}
     def handle_SAY(self, req): #{{{
         txt = " ".join(req["words"][1:])
 	voice = "rich"
     	if len(req["words"]) > 1 and req["words"][1].startswith("(") and req["words"][1].endswith(")"):
-	    txt = " ".join(req["words"][2:])
-	    voice = req["words"][1][1:-1]
+	    v = req["words"][1][1:-1]
+	    if v in allvoices:
+		txt = " ".join(req["words"][2:])
+		voice = v
+	    else:
+	    	self.sendReply(req, "I don't know voice \"%s\", use !voices to get a list." % v)
+		return
 	     
-        print "Asked to say: %s" % txt
+        print "Asked to say as %s: %s" % (voice, txt)
 	TextToSpeech(DATAFILE).add(txt, speaker="%s16" % voice)
 #}}}
     def handle_catchall(self, req): #{{{
